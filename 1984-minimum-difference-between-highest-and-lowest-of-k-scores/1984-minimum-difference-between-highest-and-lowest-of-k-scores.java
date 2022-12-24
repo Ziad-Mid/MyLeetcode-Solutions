4f4ed7d9a7cfd72cf4f5e87bbc4e1fa0252d1b23
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2022-12-24 02:50:38 +0100

    Time: 4 ms (100.00%), Space: 42.8 MB (77.73%) - LeetHub

---
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        int i = 0;
+        int j = k-1;
+        int min = Integer.MAX_VALUE ;
+                      
+        if(k == 1)
+            return 0;
+                  
+        Arrays.sort(nums);
+        
+        while(j < nums.length){
+            min = Math.min(min,nums[j] - nums[i]);
+            j++;
+            i++;
+        }
+
+        return min;
+    }
+}
