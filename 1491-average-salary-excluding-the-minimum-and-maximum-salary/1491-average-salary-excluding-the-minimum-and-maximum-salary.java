69e1874d615eaa4bfd19288942392d434b8734fe
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2022-12-25 01:04:57 +0100

    Time: 0 ms (100.00%), Space: 40.2 MB (74.90%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.java
@@ -0,0 +1,16 @@
+class Solution {
+    public double average(int[] salary) {
+        
+        int sum = 0;
+        
+        Arrays.sort(salary);
+        
+        for(int i = 1 ; i < salary.length - 1 ; i++){
+            sum+=salary[i];
+        }
+        
+     
+        
+        return (double) sum / (salary.length-2);
+    }
+}
