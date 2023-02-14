51329db32fb456ae88d77002570e9c9ddff2088f
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-02-14 01:18:05 +0100

    Time: 176 ms (14.87%), Space: 42.8 MB (50.81%) - LeetHub

---
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int n = nums.length ;
+        int count = 0;
+        if(n == 1)
+            return 0;
+        
+            for(int i = 1 ; i < n ; i++){
+                System.out.println(nums[i]+" "+nums[i-1]);
+                if(nums[i] == nums[i-1])
+                {
+                    
+                    count += 1 ;
+                    nums[i]++ ; 
+                }    
+                else if(nums[i]<nums[i-1])
+                {
+                    count += nums[i-1] - nums[i] + 1 ;
+                    nums[i] = nums[i-1] + 1 ;
+                }
+            
+        }
+        
+        return count;
+    }
+}
