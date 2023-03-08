a6d089b4e192d00c26647de09939c426a0740a79
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-03-08 01:58:44 +0100

    Time: 2 ms (86.22%), Space: 43.2 MB (41.17%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
@@ -18,21 +18,6 @@ class Solution {
             }
         }
         
-        
-        
-        System.out.println(l.toString());
         return l;
     }
 }
-
-// char cs=s.charAt(0),ce=s.charAt(3);
-//     char rs=s.charAt(1),re=s.charAt(4);
-
-//     List<String> ls=new ArrayList<>();
-
-//     for(char c=cs;c<=ce;c++){
-//         for(char r=rs;r<=re;r++){
-//             ls.add(new String(new char[]{c,r}));
-//         }
-//     }
-//     return ls;
