31d6e1472f9612e76bdb9bcf67b5d84e3cc60968
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-03-08 01:58:27 +0100

    Time: 5 ms (73.47%), Space: 43.3 MB (41.17%) - LeetHub

--- 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
+++ 2194-cells-in-a-range-on-an-excel-sheet/2194-cells-in-a-range-on-an-excel-sheet.java
@@ -5,17 +5,16 @@ class Solution {
         
         String[] words = s.split(":");
         char col1 = words[0].charAt(0);
-        int row1 = words[0].charAt(1) - 48;
+        char row1 = words[0].charAt(1);
         
         char col2 = words[1].charAt(0);
-        int row2 = words[1].charAt(1) - 48;
+        char row2 = words[1].charAt(1);
         
-        for(int c = col1 ; c <= col2 ; c++){
-            for(int r = row1 ; r <= row2 ; r++){
-                char col = (char) c;
+        for(char c = col1 ; c <= col2 ; c++){
+            for(char r = row1 ; r <= row2 ; r++){
+                char col = c;
                 
-                String cell = col+""+r;
-                l.add(cell);
+                l.add(new String(new char[]{col,r}));
             }
         }
         
@@ -25,3 +24,15 @@ class Solution {
         return l;
     }
 }
+
+// char cs=s.charAt(0),ce=s.charAt(3);
+//     char rs=s.charAt(1),re=s.charAt(4);
+
+//     List<String> ls=new ArrayList<>();
+
+//     for(char c=cs;c<=ce;c++){
+//         for(char r=rs;r<=re;r++){
+//             ls.add(new String(new char[]{c,r}));
+//         }
+//     }
+//     return ls;
