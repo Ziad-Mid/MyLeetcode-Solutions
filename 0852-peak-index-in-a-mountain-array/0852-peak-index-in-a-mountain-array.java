ede3dadd9c8d091ea953670e844310df3a58693a
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-04-30 14:42:49 +0100

    Time: 0 ms (100.00%), Space: 58.5 MB (69.89%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,19 +1,23 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         
-       int i = 0;
+        int start = 0;
+        int end = arr.length - 1;
         
-        int res = 0;
+        //dichotomic search
         
-        while(i<arr.length){
-            if(arr[i]< arr[i+1]){
-                i++;
-                continue;
-            }
+        while (start <= end) {
+            //get the middle index
+            int middle = start + (end - start) / 2;
             
-            res=i;
-            break;
+            //check if the max number is on the right or left side of the middle number of the array
+            if (arr[middle] < arr[middle + 1]) {
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
         }
-        return res;
+
+        return start;
     }
 }
