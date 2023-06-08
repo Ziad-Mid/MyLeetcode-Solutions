1558c3e209f037c0bbed0772606a7a58b557a425
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-06-08 19:04:14 +0100

    Time: 1 ms (99.62%), Space: 48.7 MB (14.56%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,25 +1,19 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
-        Map<Integer,Integer> hm = new HashMap<>();
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i],hm.get(nums[i])+1);
-            }
-            else{
-                hm.put(nums[i],1);
-            }
-        }
-        int max = 0;
-        int res = 0;
-        for(Map.Entry<Integer, Integer> key : hm.entrySet()){
-            if(key.getValue()> max){
-                res = key.getKey();
-                max = key.getValue();
-            }
-        }
-        
-        return res;
+       int count = 0, maxElement = 0;
+      
+				for(int num: nums) {
+					if(count == 0) {
+						maxElement = num;
+					}
+					if(num == maxElement) {
+						count++;
+					} else {
+						count--;
+					}
+				}
+
+				return maxElement;
     }
 }
