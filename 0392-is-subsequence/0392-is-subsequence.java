f8711c743eea5e4e4aa0c781dc95eaba88447ffe
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-06-16 18:52:53 +0100

    Time: 0 ms (100.00%), Space: 40.7 MB (57.89%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,11 +1,29 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
         
+//         int i = 0 ;
+//         int currentSearchable = 0 ;
+        
+//         while(i < t.length() && currentSearchable< s.length() ){
+//             if(t.charAt(i) == s.charAt(currentSearchable)){
+//                 currentSearchable++;
+//             }
+//             i++;
+//         }
+        
+//         return currentSearchable == s.length();
+        
+        
+        //0 ms solution
         int i = 0 ;
         int currentSearchable = 0 ;
+        int n=t.length();
+        int m=s.length();
+        char charArrayS[]=s.toCharArray();
+        char charArrayT[]=t.toCharArray();
         
-        while(i < t.length() && currentSearchable< s.length() ){
-            if(t.charAt(i) == s.charAt(currentSearchable)){
+        while(i < n && currentSearchable< m ){
+            if(charArrayT[i] == charArrayS[currentSearchable]){
                 currentSearchable++;
             }
             i++;
