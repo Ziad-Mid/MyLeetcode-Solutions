116a2b9dfa886e7cdafa3708b8aee2fb08626072
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-06-16 18:36:07 +0100

    Time: 3 ms (9.91%), Space: 40.6 MB (72.86%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -3,24 +3,16 @@ class Solution {
         
         int i = 0 ;
         int currentSearchable = 0 ;
-        Stack<Character> stack = new Stack<Character>();
         
         while(i < t.length() && currentSearchable< s.length() ){
             Character cT = t.charAt(i);
             Character cS = s.charAt(currentSearchable);
             if(cT.equals(cS)){
-                stack.push(cT);
                 currentSearchable++;
-                
             }
-            
             i++;
         }
         
-        return stack.size() == s.length() ? true : false;
+        return currentSearchable == s.length();
     }
 }
-
-//stack = a
-//k =1
-//
