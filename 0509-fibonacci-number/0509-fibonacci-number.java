a52a1c75e8b1fa02ce069c35129c31f55167af68
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-07-07 18:11:26 +0100

    Time: 0 ms (100.00%), Space: 38.9 MB (97.83%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,18 @@
 class Solution {
     public int fib(int n) {
-        if(n==0||n==1) {
-            return n;
+         if(n == 1) {
+            return 1;
         }
-        return fib(n-1)+fib(n-2);
+
+        int prevprev = 0;
+        int prev = 1;
+        int cur = 0;
+
+        for(int i = 2; i <=n; i++) {
+            cur = prevprev + prev;
+            prevprev = prev;
+            prev = cur;
+        }
+        return cur;
     }
 }
