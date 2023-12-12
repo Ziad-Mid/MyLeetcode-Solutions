c9e231877ea09346916bd2c8c95c7db7c63f685c
El Midaoui Ziad <ziad.elmidaoui@edu.uiz.ac.ma>
2023-12-12 18:38:32 +0100

    Time: 56 ms (53.75%), Space: 42.7 MB (37.50%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+    let stack = [];
+    
+    for(let ch of s){
+        if(ch === '(' || ch === '[' || ch === '{'){
+            stack.push(ch);
+        }
+        
+        else {
+            if (!stack.length ||
+                (ch === ')' && stack[stack.length - 1] !== '(') || 
+                (ch === '}' && stack[stack.length - 1] !== '{') ||
+                (ch === ']' && stack[stack.length - 1] !== '[')) {
+                return false; 
+            }
+            
+            stack.pop()
+        }
+    }
+    
+    return !stack.length
+};
+
+
